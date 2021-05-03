97bb976246e5111be749ad820182a57e6096ae15
Ariel Wernick <arielwernick@gmail.com>
2021-05-03 14:59:11 -0400

    to view

---
+++ virtualSchoolPortalProject/src/main/java/sepractice/Instructor.java
@@ -0,0 +1,45 @@
+package sepractice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Instructor extends Patron {
+
+
+    /*
+    Instructor class: Instructor attributes
+      A)First Name
+      B)last Name
+      C) id number
+      D) department
+      E) phone number
+      I) email address
+      //to improve make the above into a parent class called patron and have this class inherit those methods
+      E)  classes currently teaching
+      F)classes previously taught
+      G) department
+      H) phone number
+      I) email address
+      H)
+     */
+    private String department;
+   private Set<Course> courseList = new HashSet<>();
+
+    public Instructor(String firstName, String lastName, String emailAddress, String password,String phoneNumber, long idNumber,String department){
+        super(firstName,lastName,emailAddress,password,phoneNumber,idNumber);
+        this.department = department;
+
+    }
+
+    public Course addCourse(Course course){
+        courseList.add(course);
+        return course;
+    }
+
+    public Set<Course> getCourseList(){
+        return courseList;
+    }
+
+
+
+}
