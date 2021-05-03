c0aa15cf4e6a9fd7fe9c43359ab332586d566658
Ariel Wernick <arielwernick@gmail.com>
2021-05-03 14:25:21 -0400

    second upload

---
+++ virtualSchoolPortalProject/src/main/java/sepractice/Administator.java
@@ -0,0 +1,48 @@
+package sepractice;
+public class Administator extends Patron {
+   /*
+    admin class: admin attributes
+   inherited from patron:
+    A)First Name
+    B)last Name
+     F) phone number
+    G) email address
+
+    -id number
+    D) Department
+    E) if instructor as well, instructor profile
+    F) phone number
+    G) email address
+    H)
+
+    admin abilities:
+    create course
+    delete course
+    create new student
+
+            */
+
+    private String firstName;
+    private String lastName;
+    private Long idNumber;
+    private String emailAddress;
+    private Long phoneNumber;
+
+    public Administator(String firstName, String lastName, String emailAddress,String password, String phoneNumber,long idNumber) {
+        super(firstName, lastName, emailAddress,password, phoneNumber,idNumber);
+    }
+
+
+    private long setIDNumber() {
+        return 0000;
+    }
+
+    public Course createCourse(){
+        return null;
+    }
+
+
+}
+
+
+
